b6067f4b6109a38f7fe05acfc0c50bf4a57b9b00
borgaonkarjui <borgaonkarjui@gmail.com>
2026-04-20 01:33:45 -0400

    Evaluating model with snapshon and global rmse

--- utils/modelEvaluation.py
+++ utils/modelEvaluation.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from utils.ExploratoryDataAnalysis import fetch_sensor_cols
 
 #plot training curves
 def plot_training_curves(model):
@@ -14,14 +17,11 @@ def plot_training_curves(model):
     
     plt.figure(figsize=(10, 6))
     
-    # Plot validation RMSE
-    plt.plot(x_axis, results['validation_0']['rmse'], label='Validation')
+    # Plot train RMSE
+    plt.plot(x_axis, results['validation_0']['rmse'], label='Train')
     
-    # Plot training RMSE (if available in the eval_set)
-    if 'validation_1' in results:
-        plt.plot(x_axis, results['validation_1']['rmse'], label='Train')
-    elif 'train' in results:
-        plt.plot(x_axis, results['train']['rmse'], label='Train')
+    # Plot validation RMSE
+    plt.plot(x_axis, results['validation_1']['rmse'], label='Validation')
         
     plt.title('XGBoost Regression Error (RMSE) over Time')
     plt.xlabel('Number of Trees (Iterations)')
@@ -53,3 +53,34 @@ def plot_feature_importance(model, importance_type='gain'):
     plt.xlabel(f'Relative {importance_type.capitalize()} Score')
     plt.grid(axis='x', linestyle='--', alpha=0.7)
     plt.show()
+
+#evaluate rmse
+def eval_rmse(model, X_test, y_true_test):
+    # Predict RUL for the test engines snapshot
+    y_pred_test = model.predict(X_test)
+
+    # Clip predictions (RUL cannot be negative)
+    y_pred_test = y_pred_test.clip(min=0)
+
+    # Calculate RMSE
+    test_rmse = np.sqrt(mean_squared_error(y_true_test, y_pred_test))
+
+    print(f"Final Test RMSE on FD004: {test_rmse:.2f} cycles")
+
+    return y_pred_test, test_rmse
+
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+#plot results y_true vs y_pred
+def plot_test_results(y_true, y_pred):
+    plt.figure(figsize=(10, 6))
+    plt.plot(y_true.values, label='Actual RUL', color='blue', marker='o', markersize=3, linestyle='')
+    plt.plot(y_pred, label='Predicted RUL', color='red', marker='x', markersize=3, linestyle='')
+    plt.title('Actual vs Predicted RUL (Test Set FD004)')
+    plt.xlabel('Engine ID')
+    plt.ylabel('Remaining Useful Life (Cycles)')
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.show()
+
