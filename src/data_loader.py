9a31d1b087a0c939e783262e624f0c430dbe8082
borgaonkarjui <borgaonkarjui@gmail.com>
2026-04-20 01:33:07 -0400

    Adding test data loader with snapshot and global loading options

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
+from utils.DataLoading import load_rul_truth
 
-def xgboost_data_loader(train_path, val_path, 
+def xgboost_train_loader(train_path, val_path, 
     selected_sensors=['T24', 'T30', 'T50', 'P30', 'Nf', 'Nc', 'epr', 'Ps30', 'NRf', 'NRc', 'htBleed'], 
     target_col='target_rul'
     ):
@@ -20,3 +22,52 @@ def xgboost_data_loader(train_path, val_path,
     y_val = val_df[target_col]
     
     return X_train, y_train, X_val, y_val
+
+def prepare_test_data(test_df, selected_sensors):
+    """
+    Extracts the last available cycle for each engine in the test set.
+    """
+    # Get the last row for each unit_id
+    test_last_cycles = test_df.groupby('unit_id').last().reset_index()
+    
+    # Separate features (X_test)
+    features = ['op_regime'] + selected_sensors
+    X_test = test_last_cycles[features]
+    
+    return X_test, test_last_cycles['unit_id']
+
+def xgboost_test_loader(test_path, rul_path,
+                        selected_sensors=['T24', 'T30', 'T50', 'P30', 'Nf', 'Nc', 'epr', 'Ps30', 'NRf', 'NRc', 'htBleed'],
+                        is_snapshot=True,
+                        ):
+    #load test data
+    test_df = pd.read_csv(test_path)
+    y_test = load_rul_truth(rul_path)
+    features = ['op_regime'] + selected_sensors
+
+    if is_snapshot:
+        # Get the last row for each unit_id
+        test_last_cycles = test_df.groupby('unit_id').last().reset_index()
+        # Separate features (X_test)
+        X_test = test_last_cycles[features]
+
+    else:
+        # Create unit_id col for y_test
+        y_test['unit_id'] = range(1, len(y_test) + 1)
+        
+        # Get max cycle for each engine in test
+        max_cycles = test_df.groupby('unit_id')['cycle'].max().reset_index()
+        max_cycles.columns = ['unit_id', 'max_cycle']
+
+        # Merge truth into main test dataframe
+        df_merged = test_df.merge(y_test, on='unit_id').merge(max_cycles, on='unit_id')
+
+        # Back-calculate the RUL for every row
+        # Formula: Current_True_RUL = Final_True_RUL + (Max_Cycle - Current_Cycle)
+        df_merged['true_rul_actual'] = df_merged['true_rul'] + (df_merged['max_cycle'] - df_merged['cycle'])
+
+        y_test = df_merged['true_rul_actual']
+        X_test = df_merged[features]
+    
+    return X_test, y_test
+
